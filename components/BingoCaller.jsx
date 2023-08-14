b146a1390589d87f218e1987302cf121a7bab288
Hank Kapka <hankkapka@gmail.com>
2023-08-14 16:32:09 -0500

    updated caller function to match specifics of the game type

--- components/BingoCaller.jsx
+++ components/BingoCaller.jsx
@@ -26,18 +26,24 @@ import React, { useState } from 'react';
 const BingoCaller = () => {
     const [calledNumbers, setCalledNumbers] = useState([]);
 
-    const letters = ['W', 'A', 'L', 'D', 'O'];
+    const letters = ['B', 'I', 'N', 'G', 'O'];
 
     const callBingoNumber = () => {
-        const minNumber = 1;
-        const maxNumber = 75;
+        const letterRanges = {
+            'B': { min: 1, max: 15 },
+            'I': { min: 16, max: 31 },
+            'N': { min: 32, max: 46 },
+            'G': { min: 47, max: 61 },
+            'O': { min: 62, max: 75 },
+        };
 
-        // Generate a new Bingo Call that hasn't been called before
         let bingoNumber;
         do {
             const letterIndex = Math.floor(Math.random() * letters.length);
-            const number = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-            bingoNumber = letters[letterIndex] + number;
+            const letter = letters[letterIndex];
+            const { min, max } = letterRanges[letter];
+            const number = Math.floor(Math.random() * (max - min + 1)) + min;
+            bingoNumber = `${letter}-${number}`;
         } while (calledNumbers.includes(bingoNumber));
 
         // Update the state to include the new Bingo Call
@@ -46,21 +52,20 @@ const BingoCaller = () => {
     };
 
     return (
-        <div className="p-6">
-            <button className="flex flex-row relative justify-center mx-auto items-center py-4 px-6 outline rounded-3xl" onClick={callBingoNumber}>DRAW</button>
+        <div>
+            <button onClick={callBingoNumber}>Call Bingo Number</button>
             <div className="">
                 <p className="sm:text-[64px] text-[50px] font-extrabold flex flex-row relative justify-center items-center py-4 px-6">{calledNumbers.length > 0 ? calledNumbers[calledNumbers.length - 1] : '-'}</p>
             </div>
-
             {letters.map((letter) => {
                 const numbersForLetter = calledNumbers
-                    .filter((bingoNumber) => bingoNumber.charAt(0) === letter)
-                    .sort((a, b) => parseInt(a.slice(1)) - parseInt(b.slice(1))); // Sort numerically
+                    .filter((bingoNumber) => bingoNumber.startsWith(letter))
+                    .sort((a, b) => parseInt(a.split('-')[1]) - parseInt(b.split('-')[1])); // Sort numerically
 
                 return (
-                    <div className="grid grid-cols-3 justify-center w-full gap-8 pt-8" key={letter}>
-                        <h2 className="sm:text-[64px] text-[50px] font-extrabold flex flex-row relative justify-center items-center py-4 px-6">{letter}</h2>
-                        <ul className="flex flex-col p-2 justify-center items-center text-black-100 hover:bg-white hover:text-black hover:shadow-md rounded-3xl">
+                    <div key={letter}>
+                        <h2>{letter}</h2>
+                        <ul>
                             {numbersForLetter.map((bingoNumber, index) => (
                                 <li key={index}>{bingoNumber}</li>
                             ))}
