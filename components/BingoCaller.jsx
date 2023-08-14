8675d4f9e7790df5b03089aac9ffd485246611b3
Hank Kapka <hankkapka@gmail.com>
2023-08-14 16:21:29 -0500

    updated to group by calledLetter then number

--- components/BingoCaller.jsx
+++ components/BingoCaller.jsx
@@ -1,10 +1,34 @@
+//     return (
+//         <div className="p-6">
+//             <button
+//                 className="flex flex-row relative justify-center mx-auto items-center py-4 px-6 outline rounded-3xl"
+//                 onClick={callBingoNumber}>DRAW</button>
+
+//             <div className="">
+//                 <p className="sm:text-[64px] text-[50px] font-extrabold flex flex-row relative justify-center items-center py-4 px-6">{calledNumbers.length > 0 ? calledNumbers[calledNumbers.length - 1] : '-'}</p>
+//             </div>
+//             <ul className="">
+//                 <div className="grid grid-cols-3 justify-center w-full gap-8 pt-8">
+//                     {calledNumbers.map((bingoNumber, index) => (
+//                         <li key={index}
+//                             className="flex flex-col p-2 justify-center items-center text-black-100 hover:bg-white hover:text-black hover:shadow-md rounded-3xl">{bingoNumber}</li>
+//                     ))}
+//                 </div>
+//             </ul>
+//         </div>
+//     );
+// };
+
+// export default BingoCaller;
+
 import React, { useState } from 'react';
 
 const BingoCaller = () => {
     const [calledNumbers, setCalledNumbers] = useState([]);
 
+    const letters = ['W', 'A', 'L', 'D', 'O'];
+
     const callBingoNumber = () => {
-        const letters = ['W', 'A', 'L', 'D', 'O'];
         const minNumber = 1;
         const maxNumber = 75;
 
@@ -17,25 +41,33 @@ const BingoCaller = () => {
         } while (calledNumbers.includes(bingoNumber));
 
         // Update the state to include the new Bingo Call
-        setCalledNumbers([...calledNumbers, bingoNumber]);
+        const newCalledNumbers = [...calledNumbers, bingoNumber];
+        setCalledNumbers(newCalledNumbers);
     };
 
     return (
         <div className="p-6">
-            <button
-                className="flex flex-row relative justify-center mx-auto items-center py-4 px-6 outline rounded-3xl"
-                onClick={callBingoNumber}>DRAW</button>
+            <button className="flex flex-row relative justify-center mx-auto items-center py-4 px-6 outline rounded-3xl" onClick={callBingoNumber}>DRAW</button>
             <div className="">
                 <p className="sm:text-[64px] text-[50px] font-extrabold flex flex-row relative justify-center items-center py-4 px-6">{calledNumbers.length > 0 ? calledNumbers[calledNumbers.length - 1] : '-'}</p>
             </div>
-            <ul className="">
-                <div className="grid grid-cols-3 justify-center w-full gap-8 pt-8">
-                    {calledNumbers.map((bingoNumber, index) => (
-                        <li key={index}
-                            className="flex flex-col p-2 justify-center items-center text-black-100 hover:bg-white hover:text-black hover:shadow-md rounded-3xl">{bingoNumber}</li>
-                    ))}
-                </div>
-            </ul>
+
+            {letters.map((letter) => {
+                const numbersForLetter = calledNumbers
+                    .filter((bingoNumber) => bingoNumber.charAt(0) === letter)
+                    .sort((a, b) => parseInt(a.slice(1)) - parseInt(b.slice(1))); // Sort numerically
+
+                return (
+                    <div className="grid grid-cols-3 justify-center w-full gap-8 pt-8" key={letter}>
+                        <h2 className="sm:text-[64px] text-[50px] font-extrabold flex flex-row relative justify-center items-center py-4 px-6">{letter}</h2>
+                        <ul className="flex flex-col p-2 justify-center items-center text-black-100 hover:bg-white hover:text-black hover:shadow-md rounded-3xl">
+                            {numbersForLetter.map((bingoNumber, index) => (
+                                <li key={index}>{bingoNumber}</li>
+                            ))}
+                        </ul>
+                    </div>
+                );
+            })}
         </div>
     );
 };
